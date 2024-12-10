b78320044c5ef727d7b3878f727b25e5dd880fc0
Mast0 <56480106+WJIaDbIka@users.noreply.github.com>
2024-12-11 01:15:37 +0200

    #6 Create Unit Of Work
    create interface and class UnitOfWork

---
+++ ControlAccessSystemNP/DataLayer/UnitOfWork/Interface/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DataLayer.Repository.Interfaces;
+
+namespace DataLayer.UnitOfWork.Interface
+{
+	public interface IUnitOfWork
+	{
+		IAdministratorRepository AdministratorRepository { get; }
+
+		IWorkerRepository WorkerRepository { get; }
+
+		ICardRepository CardRepository { get; }
+
+		Task SaveAsync();
+	}
+}
---
+++ ControlAccessSystemNP/DataLayer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using DataLayer.EF;
+using DataLayer.Repository;
+using DataLayer.Repository.Interfaces;
+using DataLayer.UnitOfWork.Interface;
+
+namespace DataLayer.UnitOfWork
+{
+	public sealed class UnitOfWork : IUnitOfWork
+	{
+		private readonly ControlAccessSystemContext _context;
+		private readonly IAdministratorRepository _administratorRepository;
+		private readonly IWorkerRepository _workerRepository;
+		private readonly ICardRepository _cardRepository;
+
+        public UnitOfWork(ControlAccessSystemContext context)
+        {
+            _context = context;
+			_administratorRepository = new AdministratorRepository(context);
+			_workerRepository = new WorkerRepository(context);
+			_cardRepository = new CardRepository(context);
+        }
+
+		public IAdministratorRepository AdministratorRepository => _administratorRepository;
+
+		public IWorkerRepository WorkerRepository => _workerRepository;
+
+		public ICardRepository CardRepository => _cardRepository;
+
+		public async Task SaveAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+	}
+}
